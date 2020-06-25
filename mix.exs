05d1956ae4b1fa3932a5ee9dd4c8818335d436fe
Bruce Williams <brwcodes@gmail.com>
2020-06-24 23:26:47 -0700

    Better boilerplace (don't need app)
    Also adds ex_doc and some basic package information.

--- mix.exs
+++ mix.exs
@@ -7,23 +7,42 @@ defmodule Cased.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package()
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
-      mod: {Cased.Application, []}
+      extra_applications: [:logger]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, "~> 0.22", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp package do
+    [
+      description: "Cased provides user and employee activity audit trails to companies that need to monitor access to information or demonstrate regulatory compliance.",
+      files: [
+        "lib",
+        "mix.exs",
+        "README.md",
+        ".formatter.exs"
+      ],
+      licenses: ["MIT"],
+      links: %{
+        Website: "https://cased.com",
+        GitHub: "https://github.com/cased/cased-elixir",
+        Changelog: "https://github.com/cased/cased-elixir/releases"
+      }
     ]
   end
 end
